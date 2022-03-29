10dcebafc55cb4d9f6a55aa00db3f15f0813a895
Carson Weeks <CarsonWeeks@mail.com>
2022-03-28 22:31:55 -0700

    inital

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\InvoiceController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::resource('/customer', CustomerController::class);
+Route::resource('/manufacturer', ManufacturerController::class);
+Route::resource('/equipment', EquipmentController::class);
+Route::resource('/invoice', InvoiceController::class);
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
